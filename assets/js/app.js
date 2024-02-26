be3ac57daf7e32e5cb987aa514f9229646c71c33
muhammadali057 <wygqkjsaq>
2024-02-26 08:56:33 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+
+
+
+
+// const A = 5; 
+// const B = 3; 
+
+// for (let i = B + 1; i < A; i++) {
+//     console.log(i);
+// }
+
+
+
+// const A = 7; 
+// const B = 3; 
+
+// const juftSonlar = [];
+// const toqSonlar = [];
+
+// for (let i = B + 1; i < A; i++) {
+//     if (i % 2 === 0) {
+//         juftSonlar.push(i);
+//     } else {
+//         toqSonlar.push(i);
+//     }
+// }
+
+// console.log('Juft sonlar:', juftSonlar);
+// console.log('Toq sonlar:', toqSonlar);
+
+
+
+const yosh = 45; 
+
+if (yosh >= 0 && yosh <= 30) {
+    console.log("Yoshsiz");
+} else if (yosh > 30 && yosh <= 60) {
+    console.log("Kattasiz");
+} else {
+    console.log("Siz qarisiz");
+}
